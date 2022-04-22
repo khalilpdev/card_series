018d5558725c4c312e5dc8e406d64951bae15309
Leandro Khalil <khalilpdev@gmail.com>
2022-04-22 17:29:03 -0300

    fist with ListTile

--- lib/simple_card/simple_card.dart
+++ lib/simple_card/simple_card.dart
@@ -6,9 +6,19 @@ class SimpleCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 100,
-      width: 100,
-      color: Colors.red,
-    );
+        height: 100,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.red,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        padding: EdgeInsets.all(16),
+        margin: EdgeInsets.all(12),
+        child: ListTile(
+          leading: CircleAvatar(),
+          title: Text('Leandro Khalil'),
+          subtitle: Text('C# Developer Manager'),
+          trailing: Icon(Icons.camera_alt_outlined),
+        ));
   }
 }
