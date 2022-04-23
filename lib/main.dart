2a47675091aaf0a60c2918e6b4315f9680bcaebb
Leandro Khalil <khalilpdev@gmail.com>
2022-04-23 18:39:31 -0300

    finished simple cards

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Cards Example'),
     );
   }
 }
@@ -32,7 +32,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xffBACAD9),
+      backgroundColor: const Color(0xffBACAD9),
       appBar: AppBar(
         title: Text(widget.title),
       ),
@@ -40,7 +40,26 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            SimpleCard(),
+            SimpleCard(
+              title: "Leandro Khalil",
+              subtitle: "C# Developer Manager",
+              icon: Icons.arrow_forward_ios_outlined,
+              imageUrl:
+                  "https://media-exp1.licdn.com/dms/image/C4D03AQGON82icciopQ/profile-displayphoto-shrink_200_200/0/1617628995695?e=1655942400&v=beta&t=u_rLzdXO77XN18STLNp8XOmCerdSlvKi8s00dRM2T68",
+              onClick: () {
+                debugPrint("teste");
+              },
+            ),
+            SimpleCard(
+              imageUrl:
+                  ("https://cdn.pixabay.com/photo/2020/11/10/15/51/bear-5730216_960_720.png"),
+              title: "Other 1",
+              subtitle: "Brave ",
+              icon: Icons.close,
+              onClick: () {
+                debugPrint("teste 2");
+              },
+            )
           ],
         ),
       ),
--- lib/simple_card/simple_card.dart
+++ lib/simple_card/simple_card.dart
@@ -1,38 +1,58 @@
 import 'package:flutter/material.dart';
 
 class SimpleCard extends StatelessWidget {
-  const SimpleCard({Key? key}) : super(key: key);
+  final String imageUrl, title, subtitle;
+  final IconData icon;
+  final VoidCallback onClick;
+
+  const SimpleCard({
+    Key? key,
+    required this.imageUrl,
+    required this.title,
+    required this.subtitle,
+    required this.icon,
+    required this.onClick,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        height: 100,
-        width: double.infinity,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(20),
+    return Padding(
+      padding: const EdgeInsets.all(12),
+      child: Material(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+        child: InkWell(
+          onTap: (() {
+            debugPrint('teste');
+          }),
+          child: Container(
+              height: 100,
+              width: double.infinity,
+              decoration:
+                  BoxDecoration(borderRadius: BorderRadius.circular(20)),
+              padding: const EdgeInsets.all(16),
+              margin: const EdgeInsets.all(12),
+              child: ListTile(
+                contentPadding: const EdgeInsets.all(0),
+                leading: CircleAvatar(
+                  radius: 30,
+                  backgroundImage: NetworkImage(imageUrl),
+                ),
+                title: Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: Text(title),
+                ),
+                subtitle: Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: Text(subtitle),
+                ),
+                trailing: Icon(
+                  icon,
+                  size: 27,
+                ),
+              )),
         ),
-        padding: const EdgeInsets.all(16),
-        margin: const EdgeInsets.all(12),
-        child: const ListTile(
-          contentPadding: EdgeInsets.all(0),
-          leading: CircleAvatar(
-            radius: 30,
-            backgroundImage: NetworkImage(
-                "https://media-exp1.licdn.com/dms/image/C4D03AQGON82icciopQ/profile-displayphoto-shrink_200_200/0/1617628995695?e=1655942400&v=beta&t=u_rLzdXO77XN18STLNp8XOmCerdSlvKi8s00dRM2T68"),
-          ),
-          title: Padding(
-            padding: EdgeInsets.only(top: 8.0),
-            child: Text("Leandro Khalil"),
-          ),
-          subtitle: Padding(
-            padding: EdgeInsets.only(top: 8.0),
-            child: Text('C# Developer Manager'),
-          ),
-          trailing: Icon(
-            Icons.camera_alt_outlined,
-            size: 27,
-          ),
-        ));
+      ),
+    );
   }
 }
