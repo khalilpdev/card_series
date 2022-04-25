5b4633bac734fafa8d6fa93a11a4b7c4015fbfc9
Leandro Khalil <khalilpdev@gmail.com>
2022-04-24 21:42:34 -0300

    continue

--- lib/credit_card/credit_card.dart
+++ lib/credit_card/credit_card.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class CreditCard extends StatelessWidget {
   const CreditCard({Key? key}) : super(key: key);
 
+  final String bankName = "KhalilBank";
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -16,43 +18,76 @@ class CreditCard extends StatelessWidget {
           ),
           child: Padding(
             padding: const EdgeInsets.all(16),
-            child: Column(children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: const [
-                  Icon(Icons.rocket_launch_sharp, color: Colors.white),
-                  Text(
-                    'KhalilBank',
-                    style: TextStyle(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    const Icon(Icons.rocket_launch_sharp, color: Colors.white),
+                    Text(
+                      bankName,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Column(
+                      children: [
+                        Row(
+                          children: [
+                            Container(
+                              width: 40,
+                              height: 30,
+                              color: Colors.grey,
+                            ),
+                            const SizedBox(width: 16),
+                            const Icon(
+                              Icons.wifi,
+                              color: Colors.white,
+                            )
+                          ],
+                        )
+                      ],
+                    )
+                  ],
+                ),
+                Row(
+                  children: const [
+                    Text(
+                      "1234 5678 9123 4567",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 1.5),
+                    ),
+                  ],
+                ),
+                RotatedBox(
+                  quarterTurns: 3,
+                  child: Text(
+                    "Utiliza funcao Credito".toUpperCase(),
+                    style: const TextStyle(
                       color: Colors.white,
-                      fontWeight: FontWeight.bold,
+                      fontSize: 8,
                     ),
                   ),
-                ],
-              ),
-              Row(
-                children: [
-                  Column(
-                    children: [
-                      Row(
-                        children: [
-                          Container(
-                            width: 40,
-                            height: 30,
-                            color: Colors.grey,
-                          ),
-                          const SizedBox(width: 16),
-                          const Icon(
-                            Icons.wifi,
-                            color: Colors.white,
-                          )
-                        ],
-                      )
-                    ],
-                  )
-                ],
-              ),
-            ]),
+                ),
+                const Text(
+                  "VISA",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
